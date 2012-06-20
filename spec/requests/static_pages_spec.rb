336a5bbbfba7040722277e5af4a6aef9c3eaabf3
AmySelsor <a_sels@me.com>
2012-06-20 02:03:57 -0500

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,61 +1,8 @@
-require 'spec_helper'
-
-describe "Static pages" do
-    
-    let(:base_title) {"Ruby on Rails Tutorial Sample App"}
-    
-    describe "Home page" do
-        
-        it "should have the h1 'Sample App'" do
-            visit '/static_pages/home'
-            page.should have_selector('h1', :text => 'Sample App')
-        end
-        
-        it "should have the title 'Home'" do
-            visit '/static_pages/home'
-            page.should have_selector('title',
-                                      :text => "#{base_title} | Home")
-        end
+def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+        base_title
+        else
+        "#{base_title} | #{page_title}"
     end
-    
-    describe "Help page" do
-        
-        it "should have the h1 'Help'" do
-            visit '/static_pages/help'
-            page.should have_selector('h1', :text => 'Help')
-        end
-        
-        it "should have the title 'Help'" do
-            visit '/static_pages/help'
-            page.should have_selector('title',
-                                      :text => "#{base_title} | Help")
-        end
-    end
-    
-    describe "About page" do
-        
-        it "should have the h1 'About Us'" do
-            visit '/static_pages/about'
-            page.should have_selector('h1', :text => 'About Us')
-        end
-        
-        it "should have the title 'About Us'" do
-            visit '/static_pages/about'
-            page.should have_selector('title',
-                                      :text => "#{base_title} | About Us")
-        end
-    end
-    
-    describe "Contacts Page" do
-        
-        it "should have h1 'Contacts Page'" do
-            visit '/static_pages/contacts'
-            page.should have_selector('h1',:text=>'Contacts')
-            end
-        it "should have title 'Contacts Page'" do
-            visit '/static_pages/contacts'
-            page.should have_selector('title',
-                                      :text=> "#{base_title} | Contacts Page")
-            end
-        end
 end
